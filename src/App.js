5d9233fcdd30806835e55cd0c59d09616a3d6ca1
stdavis <stdavis@utah.gov>
2022-04-15 14:16:36 -0600

    feat: flesh out basic layout
    Decided to mirror wasatch choice for now.
    
    add reactstrap, icons, and title font
    
    #3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import Map from '@arcgis/core/Map';
 import MapView from '@arcgis/core/views/MapView';
 import React from 'react';
+import 'typeface-montserrat';
 import './App.scss';
+import Filter from './components/Filter';
 import config from './services/config';
 
 function App() {
+  const [mapView, setMapView] = React.useState(null);
+
   React.useEffect(() => {
     const map = new Map({
       basemap: 'streets-vector',
     });
-    new MapView({ map, container: 'mapDiv', ...config.DEFAULT_EXTENT });
+    setMapView(new MapView({ map, container: 'mapDiv', ...config.DEFAULT_EXTENT }));
   }, []);
 
-  return <div id="mapDiv" className="w-100 h-100"></div>;
+  return (
+    <div className="d-flex flex-column w-100 h-100">
+      <div className="m-3 title">
+        <h4 className="my-0">RTP Projects</h4>
+      </div>
+      <div id="mapDiv" className="flex-fill border-top border position-relative">
+        <Filter mapView={mapView} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Filter.js
@@ -0,0 +1,45 @@
+import { faList } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import propTypes from 'prop-types';
+import React from 'react';
+import { Button } from 'reactstrap';
+import './Filter.scss';
+
+export default function Filter({ mapView }) {
+  const [isOpen, setIsOpen] = React.useState(true);
+  const buttonDiv = React.useRef(null);
+
+  const toggle = () => setIsOpen((current) => !current);
+
+  React.useEffect(() => {
+    if (mapView && buttonDiv.current) {
+      mapView.ui.add(buttonDiv.current, 'top-left');
+    }
+  }, [mapView, buttonDiv]);
+
+  return (
+    <>
+      <div className="esri-widget--button" ref={buttonDiv} onClick={toggle} title="Toggle Filter">
+        <FontAwesomeIcon icon={faList} />
+      </div>
+      {isOpen && (
+        <div className="card position-absolute top-0 end-0 filter-card me-3 mt-3">
+          <div className="card-header d-flex justify-content-between align-items-center">
+            Filter
+            <div className="d-flex align-items-center">
+              <Button className="reset-button text-decoration-none" color="link" onClick={() => {}}>
+                <small>reset</small>
+              </Button>
+              <Button close onClick={toggle} />
+            </div>
+          </div>
+          <div className="card-body">body</div>
+        </div>
+      )}
+    </>
+  );
+}
+
+Filter.propTypes = {
+  mapView: propTypes.object,
+};
