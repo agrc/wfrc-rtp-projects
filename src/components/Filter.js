df4c43c73d6a96d43b9ae6ae01a5a7a40f5c08bc
stdavis <stdavis@utah.gov>
2022-04-21 13:59:13 -0600

    fix: fix filter toggle

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,5 +1,6 @@
 import { faList } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import clsx from 'clsx';
 import propTypes from 'prop-types';
 import React from 'react';
 import { Button } from 'reactstrap';
@@ -22,20 +23,28 @@ export default function Filter({ mapView }) {
       <div className="esri-widget--button" ref={buttonDiv} onClick={toggle} title="Toggle Filter">
         <FontAwesomeIcon icon={faList} />
       </div>
-      {isOpen && (
-        <div className="card position-absolute top-0 end-0 filter-card me-3 mt-3">
-          <div className="card-header d-flex justify-content-between align-items-center">
-            Filter
-            <div className="d-flex align-items-center">
-              <Button className="reset-button text-decoration-none" color="link" onClick={() => {}}>
-                <small>reset</small>
-              </Button>
-              <Button close onClick={toggle} />
-            </div>
+      <div
+        className={clsx(
+          'card',
+          'position-absolute',
+          'top-0',
+          'end-0',
+          'filter-card',
+          'me-3 mt-3',
+          !isOpen && 'invisible'
+        )}
+      >
+        <div className="card-header d-flex justify-content-between align-items-center">
+          Filter
+          <div className="d-flex align-items-center">
+            <Button className="reset-button text-decoration-none" color="link" onClick={() => {}}>
+              <small>reset</small>
+            </Button>
+            <Button close onClick={toggle} />
           </div>
-          <div className="card-body">body</div>
         </div>
-      )}
+        <div className="card-body">body</div>
+      </div>
     </>
   );
 }
