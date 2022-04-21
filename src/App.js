51da6e67ea4433cfe58b79f077cb8d5f09101a86
stdavis <stdavis@utah.gov>
2022-04-21 10:28:59 -0600

    feat: add sherlock

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import * as watchUtils from '@arcgis/core/core/watchUtils';
 import Map from '@arcgis/core/Map';
 import MapView from '@arcgis/core/views/MapView';
 import Home from '@arcgis/core/widgets/Home';
@@ -5,10 +6,12 @@ import React from 'react';
 import 'typeface-montserrat';
 import './App.scss';
 import Filter from './components/Filter';
+import { MapServiceProvider, Sherlock } from './components/Sherlock';
 import config from './services/config';
 
 function App() {
   const [mapView, setMapView] = React.useState(null);
+  const [zoomToGraphic, setZoomToGraphic] = React.useState(null);
 
   React.useEffect(() => {
     const map = new Map({
@@ -20,6 +23,71 @@ function App() {
     setMapView(view);
   }, []);
 
+  const [displayedZoomGraphic, setDisplayedZoomGraphic] = React.useState(null);
+  const zoomTo = React.useCallback(
+    async (zoomObj) => {
+      if (!Array.isArray(zoomObj.target)) {
+        zoomObj.target = [zoomObj.target];
+      }
+
+      if (!zoomObj.zoom) {
+        if (zoomObj.target.every((graphic) => graphic.geometry.type === 'point')) {
+          zoomObj = {
+            target: zoomObj.target,
+            zoom: 10,
+          };
+        } else {
+          zoomObj = {
+            target: zoomObj.target,
+          };
+        }
+      }
+
+      await mapView.goTo(zoomObj);
+
+      if (displayedZoomGraphic) {
+        mapView.graphics.removeMany(displayedZoomGraphic);
+      }
+
+      setDisplayedZoomGraphic(zoomObj.target);
+
+      mapView.graphics.addMany(zoomObj.target);
+
+      if (!zoomObj.preserve) {
+        watchUtils.once(mapView, 'extent', () => {
+          mapView.graphics.removeAll();
+        });
+      }
+    },
+    [displayedZoomGraphic, mapView]
+  );
+
+  React.useEffect(() => {
+    if (zoomToGraphic) {
+      const { graphic, level, preserve } = zoomToGraphic;
+
+      graphic &&
+        zoomTo({
+          target: graphic,
+          zoom: level,
+          preserve: preserve,
+        });
+    }
+  }, [zoomToGraphic, mapView, zoomTo]);
+
+  const onSherlockMatch = (graphics) => {
+    setZoomToGraphic({
+      graphic: graphics,
+      preserve: false,
+    });
+  };
+
+  const sherlockConfig = {
+    provider: new MapServiceProvider(config.SHERLOCK.serviceUrl, config.SHERLOCK.searchField),
+    placeHolder: 'Search...',
+    onSherlockMatch,
+  };
+
   return (
     <div className="d-flex flex-column w-100 h-100">
       <div className="m-3 title">
@@ -27,6 +95,7 @@ function App() {
       </div>
       <div id="mapDiv" className="flex-fill border-top border position-relative">
         <Filter mapView={mapView} />
+        <Sherlock {...sherlockConfig}></Sherlock>
       </div>
     </div>
   );
--- src/services/config.js
+++ src/services/config.js
@@ -3,6 +3,10 @@ const config = {
     center: [-111.9, 40.75],
     zoom: 11,
   },
+  SHERLOCK: {
+    serviceUrl: 'https://gis.wfrc.org/arcgis/rest/services/General/ZoomToPlaceNames/FeatureServer/1',
+    searchField: 'NAME',
+  },
 };
 
 export default config;
