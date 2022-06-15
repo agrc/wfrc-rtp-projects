f5007cdf7eb9fd59d9afc88e73424477f002a004
stdavis <stdavis@utah.gov>
2022-06-15 12:13:08 -0600

    feat: initial filter by project type implementation
    #6

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,5 +1,6 @@
 import { whenOnce } from '@arcgis/core/core/watchUtils';
 import { useEffect, useState } from 'react';
+import config from '../services/config';
 
 // Cache layer lookup objects so that we don't have the crawl
 // the same map multiple times and also to prevent any cached
@@ -40,35 +41,36 @@ export const getLayersInMap = async (map) => {
   return layerNameLookup;
 };
 
-export const getLayers = async (layerNames, map) => {
-  const layerNameLookup = await getLayersInMap(map);
+export const getLayers = async (layerNames, mapView) => {
+  const layerNameLookup = await getLayersInMap(mapView.map);
 
   const layers = {};
 
-  Object.keys(layerNames).forEach((name) => {
+  for (const name of Object.keys(layerNames)) {
     const layer = layerNameLookup[layerNames[name]];
 
     if (!layer) {
       console.error(`Layer: ${layerNames[name]} not found in web map!`);
     }
 
-    layers[name] = layer;
-  });
+    layer.outFields = config.outFields;
+
+    layers[name] = await mapView.whenLayerView(layer);
+  }
 
   return layers;
 };
 
 export const useMapLayers = (mapView, layerNames) => {
-  const [layers, setLayers] = useState();
+  const [layers, setLayers] = useState(null);
 
   // reset layer lookup when the web map is changed
   useEffect(() => {
     const getLayersForNewMap = async () => {
       console.log('getLayersForNewMap');
       await whenOnce(mapView, 'ready');
-      await mapView.map.when();
 
-      const layers = await getLayers(layerNames, mapView.map);
+      const layers = await getLayers(layerNames, mapView);
       setLayers(layers);
     };
 
@@ -80,3 +82,35 @@ export const useMapLayers = (mapView, layerNames) => {
 
   return layers;
 };
+
+export const getLabelColor = (mode, layer) => {
+  const symbol = getSymbol(layer, config.symbolValues.mode[mode]);
+
+  return symbol ? symbol.color.toCss() : null;
+};
+
+export const getSymbol = (layer, value) => {
+  if (!layer) {
+    return null;
+  }
+
+  for (const info of layer.layer.renderer.uniqueValueInfos) {
+    if (info.value === value) {
+      return info.symbol;
+    }
+  }
+
+  throw new Error(
+    `Could not find symbol in layer: "${layer.layer.title}" for value: "${value}" in field: "${layer.layer.renderer.field}"!`
+  );
+};
+
+export function addOrRemove(array, value) {
+  if (array.includes(value)) {
+    array.splice(array.indexOf(value), 1);
+  } else {
+    array.push(value);
+  }
+
+  return array;
+}
--- src/services/config.js
+++ src/services/config.js
@@ -23,8 +23,118 @@ const config = {
     },
   },
   fieldNames: {
-    phase: 'Phase',
-    mode: 'Mode',
+    phase: 'phase',
+    mode: 'mode',
+  },
+  outFields: ['OBJECTID', 'phase', 'mode', 'improvement_type', 'plan_id', 'breakout', 'bike_type_text'],
+  projectTypes: {
+    road: {
+      'New Construction': {
+        points: "improvement_type = 'New Interchange'",
+        lines: "improvement_type LIKE 'New Construction%'",
+      },
+      'Lane Additions': {
+        points: null,
+        lines: "improvement_type LIKE 'Widening%' AND plan_id <> 'R-S-136'",
+      },
+      'HOT Lane Additions': {
+        points: null,
+        lines: "plan_id = 'R-S-136'",
+      },
+      Interchange: {
+        points: "improvement_type = 'Upgrade Interchange'",
+        lines: null,
+      },
+      Operational: {
+        points: null,
+        lines: "improvement_type = 'Operations' OR improvement_type = 'Re-striping'",
+      },
+      'Over/Underpass': {
+        points: "improvement_type = 'Grade-Separated Crossing'",
+        lines: null,
+      },
+      'Corridor Preservation': {
+        points: null,
+        lines: "improvement_type = 'Corridor Preservation' AND mode = 'Highway'",
+      },
+      'UDOT Owned Facility': {
+        points: "breakout = 'State'",
+        lines: "breakout = 'State'",
+      },
+      'Local Owned Facility': {
+        points: "breakout = 'Local'",
+        lines: "breakout = 'Local'",
+      },
+      'Mixed Ownership': {
+        points: "breakout = 'State/Local'",
+        lines: "breakout = 'State/Local'",
+      },
+    },
+    transit: {
+      'Commuter Rail': {
+        points: null,
+        lines: "improvement_type = 'Commuter Rail'",
+      },
+      'Light Rail': {
+        points: null,
+        lines: "improvement_type = 'Light Rail' OR improvement_type = 'Street Car'",
+      },
+      'Bus Rapid Transit': {
+        points: null,
+        lines: "improvement_type = 'Bus Rapid Transit'",
+      },
+      'Bus, Core Route': {
+        points: null,
+        lines: "improvement_type = 'Core Route'",
+      },
+      'Corridor Preservation': {
+        points: null,
+        lines: "improvement_type = 'Corridor Preservation' AND mode = 'Transit'",
+      },
+      'New Station': {
+        points: "improvement_type = 'Station'",
+        lines: null,
+      },
+      'Mobility Hub': {
+        points: "improvement_type = 'Transit Hub'",
+        lines: null,
+      },
+      'Park & Ride': {
+        points: "improvement_type = 'Park & Ride'",
+        lines: null,
+      },
+      'Maintenance Facility': {
+        points: "improvement_type = 'Maintenance'",
+        lines: null,
+      },
+    },
+    activeTransportation: {
+      'Multiuse Pathway': {
+        points: null,
+        lines: "bike_type_text = 'Parallel Bikeway Paved' OR bike_type_text = 'Parallel Bikeway Unpaved'",
+      },
+      'On-street, Protected': {
+        points: null,
+        lines: "bike_type_text = 'General Cycle Track'",
+      },
+      'On-street, Bike Lane': {
+        points: null,
+        lines: "bike_type_text = 'Bike Lane' OR bike_type_text = 'Buffered Bike Lane'",
+      },
+      'Signed Route/Sharrow': {
+        points: null,
+        lines:
+          "bike_type_text = 'General Shared Roadway' OR bike_type_text = 'Marked Shared Roadway' OR bike_type_text = 'Shoulder Bikeway' OR bike_type_text = 'Unknown Category'",
+      },
+      'Over/Underpass (AT)': {
+        points: "bike_type_text = 'Overpass' OR bike_type_text = 'Underpass'",
+        lines: null,
+      },
+      'At-Grade Crossing': {
+        points: "bike_type_text = 'At-Grade Crossing' OR bike_type_text = 'Crossing'",
+        lines: null,
+      },
+    },
   },
 };
 
