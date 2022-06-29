2b6bd7a0ff191c6bf49aca13bae23a030b13985a
stdavis <stdavis@utah.gov>
2022-06-29 16:57:02 -0600

    refactor: move project type configs to config file
    Helps with #9

--- src/services/config.js
+++ src/services/config.js
@@ -44,115 +44,6 @@ const config = {
     },
   },
   outFields: ['OBJECTID', 'phase', 'mode', 'improvement_type', 'plan_id', 'breakout', 'bike_type_text'],
-  projectTypes: {
-    road: {
-      'New Construction': {
-        points: "improvement_type = 'New Interchange'",
-        lines: "improvement_type LIKE 'New Construction%'",
-      },
-      'Lane Additions': {
-        points: null,
-        lines: "improvement_type LIKE 'Widening%' AND plan_id <> 'R-S-136'",
-      },
-      'HOT Lane Additions': {
-        points: null,
-        lines: "plan_id = 'R-S-136'",
-      },
-      Interchange: {
-        points: "improvement_type = 'Upgrade Interchange'",
-        lines: null,
-      },
-      Operational: {
-        points: null,
-        lines: "improvement_type = 'Operations' OR improvement_type = 'Re-striping'",
-      },
-      'Over/Underpass': {
-        points: "improvement_type = 'Grade-Separated Crossing'",
-        lines: null,
-      },
-      'Corridor Preservation': {
-        points: null,
-        lines: "improvement_type = 'Corridor Preservation' AND mode = 'Highway'",
-      },
-      'UDOT Owned Facility': {
-        points: "breakout = 'State'",
-        lines: "breakout = 'State'",
-      },
-      'Local Owned Facility': {
-        points: "breakout = 'Local'",
-        lines: "breakout = 'Local'",
-      },
-      'Mixed Ownership': {
-        points: "breakout = 'State/Local'",
-        lines: "breakout = 'State/Local'",
-      },
-    },
-    transit: {
-      'Commuter Rail': {
-        points: null,
-        lines: "improvement_type = 'Commuter Rail'",
-      },
-      'Light Rail': {
-        points: null,
-        lines: "improvement_type = 'Light Rail' OR improvement_type = 'Street Car'",
-      },
-      'Bus Rapid Transit': {
-        points: null,
-        lines: "improvement_type = 'Bus Rapid Transit'",
-      },
-      'Bus, Core Route': {
-        points: null,
-        lines: "improvement_type = 'Core Route'",
-      },
-      'Corridor Preservation': {
-        points: null,
-        lines: "improvement_type = 'Corridor Preservation' AND mode = 'Transit'",
-      },
-      'New Station': {
-        points: "improvement_type = 'Station'",
-        lines: null,
-      },
-      'Mobility Hub': {
-        points: "improvement_type = 'Transit Hub'",
-        lines: null,
-      },
-      'Park & Ride': {
-        points: "improvement_type = 'Park & Ride'",
-        lines: null,
-      },
-      'Maintenance Facility': {
-        points: "improvement_type = 'Maintenance'",
-        lines: null,
-      },
-    },
-    activeTransportation: {
-      'Multiuse Pathway': {
-        points: null,
-        lines: "bike_type_text = 'Parallel Bikeway Paved' OR bike_type_text = 'Parallel Bikeway Unpaved'",
-      },
-      'On-street, Protected': {
-        points: null,
-        lines: "bike_type_text = 'General Cycle Track'",
-      },
-      'On-street, Bike Lane': {
-        points: null,
-        lines: "bike_type_text = 'Bike Lane' OR bike_type_text = 'Buffered Bike Lane'",
-      },
-      'Signed Route/Sharrow': {
-        points: null,
-        lines:
-          "bike_type_text = 'General Shared Roadway' OR bike_type_text = 'Marked Shared Roadway' OR bike_type_text = 'Shoulder Bikeway' OR bike_type_text = 'Unknown Category'",
-      },
-      'Over/Underpass (AT)': {
-        points: "bike_type_text = 'Overpass' OR bike_type_text = 'Underpass'",
-        lines: null,
-      },
-      'At-Grade Crossing': {
-        points: "bike_type_text = 'At-Grade Crossing' OR bike_type_text = 'Crossing'",
-        lines: null,
-      },
-    },
-  },
 };
 
 // optional configSchema is for jest and storybook since they are clumsy when it comes to
@@ -161,6 +52,7 @@ export const setConfigs = async (appConfigs, configSchema = null) => {
   // we are fetching this rather than importing it so that it can be hosted publicly and available
   // for WFRC to reference it in their config files
   if (!configSchema) {
+    console.log('fetching config schema');
     const response = await fetch('config.schema.json');
     configSchema = await response.json();
   }
@@ -171,7 +63,7 @@ export const setConfigs = async (appConfigs, configSchema = null) => {
   try {
     validator.validate(appConfigs, configSchema, { throwError: true });
   } catch (error) {
-    console.error('There is an error in config.json!', error);
+    console.error('There is an error in config.json!', error.stack);
   }
 
   // i18n
