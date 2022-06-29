052802657ddf93ba302d0d9e789174922ac1ef20
stdavis <stdavis@utah.gov>
2022-06-29 16:56:06 -0600

    refactor: dynamically import app after config has been loaded

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
 import { setConfigs } from './services/config';
 
+console.log('fetching config.json');
 fetch('config.json')
   .then((response) => response.json())
   .then(async (appConfig) => {
     await setConfigs(appConfig);
+    const { default: App } = await import('./App');
 
+    console.log('rendering app');
     ReactDOM.createRoot(document.getElementById('root')).render(
       <React.StrictMode>
         <App />
