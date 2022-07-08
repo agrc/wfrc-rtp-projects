1b7997c3786df015f2eea83892bdad427ce683e7
stdavis <stdavis@utah.gov>
2022-07-08 16:03:48 -0600

    chore: reload app on config file change in dev

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,23 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
+function watchConfigFiles() {
+  return {
+    name: 'watch-config-files',
+    enforce: 'post',
+    handleHotUpdate({ file, server }) {
+      if (file.endsWith('.json')) {
+        server.ws.send({
+          type: 'full-reload',
+          path: '*',
+        });
+      }
+    },
+  };
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), watchConfigFiles()],
   base: './',
   test: {
     environment: 'happy-dom',
