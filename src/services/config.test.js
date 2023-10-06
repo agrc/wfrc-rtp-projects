4ecf76b261dce945da399652c8477af0ef56944b
stdavis <stdavis@utah.gov>
2023-10-06 14:52:18 -0600

    chore: add schema url

--- src/services/config.test.js
+++ src/services/config.test.js
@@ -1,11 +1,13 @@
 import { Validator } from 'jsonschema';
 import { describe, expect, it } from 'vitest';
-import appConfigs from './public/config.json';
-import configSchema from './public/config.schema.json';
+import appConfigs from '../../public/config.json';
+import configSchema from '../../public/config.schema.json';
 
 describe('config validation', () => {
   it('validates the config.json file', () => {
     const validator = new Validator();
+
+    delete appConfigs.aboutContent;
 
     const result = validator.validate(appConfigs, configSchema);
     expect(result.valid).toBe(true);
